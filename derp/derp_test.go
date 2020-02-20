giangv11/tailscale

commit 11048b893285cf601a224363f6e20ba82567caaa

derp: add docs on current protocol overview, break accept apart a bit
Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

derp/derp_server.go
@@ -92,6 +92,32 @@ func (s *Server) Accept(nc net.Conn, brw *bufio.ReadWriter) {
 	}
 }
 
+// registerClient notes that client c is now authenticated and ready for packets.
+// If c's public key was already connected with a different connection, the prior one is closed.
+func (s *Server) registerClient(c *sclient) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	old := s.clients[c.key]
+	if old == nil {
+		s.logf("derp: %s: client %x: adding connection", c.nc.RemoteAddr(), c.key)
+	} else {
+		old.nc.Close()
+		s.logf("derp: %s: client %x: adding connection, replacing %s", c.nc.RemoteAddr(), c.key, old.nc.RemoteAddr())
+	}
+	s.clients[c.key] = c
+}
+
+// unregisterClient
+func (s *Server) unregisterClient(c *sclient) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	cur := s.clients[c.key]
+	if cur == c {
+		s.logf("derp: %s: client %x: removing connection", c.nc.RemoteAddr(), c.key)
+		delete(s.clients, c.key)
+	}
+}
+
 func (s *Server) accept(nc net.Conn, brw *bufio.ReadWriter) error {
 	br, bw := brw.Reader, brw.Writer
 	nc.SetDeadline(time.Now().Add(10 * time.Second))
@@ -110,8 +136,9 @@ func (s *Server) accept(nc net.Conn, brw *bufio.ReadWriter) error {
 	// At this point we trust the client so we don't time out.
 	nc.SetDeadline(time.Time{})
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	if err := s.sendServerInfo(bw, clientKey); err != nil {
+		return fmt.Errorf("send server info: %v", err)
+	}
 
 	c := &sclient{
 		key: clientKey,
@@ -123,42 +150,12 @@ func (s *Server) accept(nc net.Conn, brw *bufio.ReadWriter) error {
 		c.info = *clientInfo
 	}
 
-	defer func() {
-		s.mu.Lock()
-		curClient := s.clients[c.key]
-		if curClient == c {
-			s.logf("derp: %s: client %x: removing connection", nc.RemoteAddr(), c.key)
-			delete(s.clients, c.key)
-		}
-		s.mu.Unlock()
-	}()
+	s.registerClient(c)
+	defer s.unregisterClient(c)
 
-	// Hold mu while we add the new client to the clients list and under
-	// the same acquisition send server info. This ensure that both:
-	// 1. by the time the client receives the server info, it can be addressed.
-	// 2. the server info is the very first
-	c.mu.Lock()
-	s.mu.Lock()
-	oldClient := s.clients[c.key]
-	s.clients[c.key] = c
-	s.mu.Unlock()
-	if err := s.sendServerInfo(c.bw, clientKey); err != nil {
-		return fmt.Errorf("send server info: %v", err)
-	}
-	c.mu.Unlock()
-
-	if oldClient == nil {
-		s.logf("derp: %s: client %x: adding connection", nc.RemoteAddr(), c.key)
-	} else {
-		oldClient.nc.Close()
-		s.logf("derp: %s: client %x: adding connection, replacing %s", nc.RemoteAddr(), c.key, oldClient.nc.RemoteAddr())
-	}
-
-	go func() {
-		if err := c.keepAlive(ctx); err != nil {
-			s.logf("derp: %s: client %x: keep alive failed: %v", nc.RemoteAddr(), c.key, err)
-		}
-	}()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go s.sendClientKeepAlives(ctx, c)
 
 	for {
 		dstKey, contents, err := s.recvPacket(c.br)
@@ -190,6 +187,12 @@ func (s *Server) accept(nc net.Conn, brw *bufio.ReadWriter) error {
 			}
 			s.mu.Unlock()
 		}
+	}
+}
+
+func (s *Server) sendClientKeepAlives(ctx context.Context, c *sclient) {
+	if err := c.keepAliveLoop(ctx); err != nil {
+		s.logf("derp: %s: client %x: keep alive failed: %v", c.nc.RemoteAddr(), c.key, err)
 	}
 }
 
@@ -312,13 +315,14 @@ type sclient struct {
 	bw *bufio.Writer
 }
 
-func (c *sclient) keepAlive(ctx context.Context) error {
+func (c *sclient) keepAliveLoop(ctx context.Context) error {
 	jitterMs, err := crand.Int(crand.Reader, big.NewInt(5000))
 	if err != nil {
 		panic(err)
 	}
 	jitter := time.Duration(jitterMs.Int64()) * time.Millisecond
 	c.keepAliveTimer = time.NewTimer(keepAlive + jitter)
+	defer c.keepAliveTimer.Stop()
 
 	for {
 		select {
@@ -338,7 +342,6 @@ func (c *sclient) keepAlive(ctx context.Context) error {
 			c.mu.Unlock()
 
 			if err != nil {
-				// TODO log
 				c.nc.Close()
 				return err
 			}

derp/derp.go
@@ -27,6 +27,30 @@ const magic = 0x44c55250
 // frameType is the one byte frame type header in frame headers.
 type frameType byte
 
+/*
+Protocol flow:
+
+Login:
+* client connects
+* server sends magic: [be_uint32(magic)]
+* server sends typeServerKey frame: 1 byte typeServerKey + 32 bytes of public key
+* client sends: (with no frameType)
+  - 32 bytes client public key
+  - 24 bytes nonce
+  - be_uint32 length of naclbox (capped at 256k)
+  - that many bytes of naclbox
+* (server verifies client is authorized)
+* server sends typeServerInfo frame byte + 24 byte nonce + beu32 len + naclbox(json)
+
+Steady state:
+* server occasionally sends typeKeepAlive. (One byte only)
+* client sends typeSendPacket byte + 32 byte dest pub key + beu32 packet len + packet bytes
+* server then sends typeRecvPacket byte + beu32 packet len + packet bytes to recipient conn
+
+TODO(bradfitz): require pings to be acknowledged; copy http2 PING frame w/ ping payload
+
+*/
+
 const (
 	typeServerKey  = frameType(0x01)
 	typeServerInfo = frameType(0x02)

commit 08484940766bfbb64e99d361c9b9363652472da8

derp: deflake test I flaked up in earlier change
I broke an invariant in 11048b893285cf601a224 (it was even nicely
documented then).

Also clean up the test a bit from while I was debugging it.

Fixes #84

Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

derp/derp_server.go
@@ -32,6 +32,7 @@ type Server struct {
 	logf       logger.Logf
 
 	mu       sync.Mutex
+	closed   bool
 	netConns map[net.Conn]chan struct{} // chan is closed when conn closes
 	clients  map[key.Public]*sclient
 }
@@ -51,6 +52,14 @@ func NewServer(privateKey key.Private, logf logger.Logf) *Server {
 
 // Close closes the server and waits for the connections to disconnect.
 func (s *Server) Close() error {
+	s.mu.Lock()
+	wasClosed := s.closed
+	s.closed = true
+	s.mu.Unlock()
+	if wasClosed {
+		return nil
+	}
+
 	var closedChs []chan struct{}
 
 	s.mu.Lock()
@@ -65,6 +74,12 @@ func (s *Server) Close() error {
 	}
 
 	return nil
+}
+
+func (s *Server) isClosed() bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.closed
 }
 
 // Accept adds a new connection to the server.
@@ -87,7 +102,7 @@ func (s *Server) Accept(nc net.Conn, brw *bufio.ReadWriter) {
 		s.mu.Unlock()
 	}()
 
-	if err := s.accept(nc, brw); err != nil {
+	if err := s.accept(nc, brw); err != nil && !s.isClosed() {
 		s.logf("derp: %s: %v", nc.RemoteAddr(), err)
 	}
 }
@@ -136,10 +151,6 @@ func (s *Server) accept(nc net.Conn, brw *bufio.ReadWriter) error {
 	// At this point we trust the client so we don't time out.
 	nc.SetDeadline(time.Time{})
 
-	if err := s.sendServerInfo(bw, clientKey); err != nil {
-		return fmt.Errorf("send server info: %v", err)
-	}
-
 	c := &sclient{
 		key: clientKey,
 		nc:  nc,
@@ -150,7 +161,18 @@ func (s *Server) accept(nc net.Conn, brw *bufio.ReadWriter) error {
 		c.info = *clientInfo
 	}
 
+	// Once the client is registered, it can start receiving
+	// traffic, but we want to make sure the first thing it
+	// receives after its frameClientInfo is our frameServerInfo,
+	// so acquire the c.mu lock (which guards writing to c.bw)
+	// while we register.
+	c.mu.Lock()
 	s.registerClient(c)
+	err = s.sendServerInfo(bw, clientKey)
+	c.mu.Unlock()
+	if err != nil {
+		return fmt.Errorf("send server info: %v", err)
+	}
 	defer s.unregisterClient(c)
 
 	ctx, cancel := context.WithCancel(context.Background())

derp/derp_test.go
@@ -14,67 +14,71 @@ import (
 	"tailscale.com/types/key"
 )
 
-func TestSendRecv(t *testing.T) {
-	const numClients = 3
-	var serverPrivateKey key.Private
-	if _, err := crand.Read(serverPrivateKey[:]); err != nil {
+func newPrivateKey(t *testing.T) (k key.Private) {
+	if _, err := crand.Read(k[:]); err != nil {
 		t.Fatal(err)
 	}
+	return
+}
+
+func TestSendRecv(t *testing.T) {
+	serverPrivateKey := newPrivateKey(t)
+	s := NewServer(serverPrivateKey, t.Logf)
+	defer s.Close()
+
+	const numClients = 3
 	var clientPrivateKeys []key.Private
-	for i := 0; i < numClients; i++ {
-		var key key.Private
-		if _, err := crand.Read(key[:]); err != nil {
-			t.Fatal(err)
-		}
-		clientPrivateKeys = append(clientPrivateKeys, key)
-	}
 	var clientKeys []key.Public
-	for _, privKey := range clientPrivateKeys {
-		clientKeys = append(clientKeys, privKey.Public())
+	for i := 0; i < numClients; i++ {
+		priv := newPrivateKey(t)
+		clientPrivateKeys = append(clientPrivateKeys, priv)
+		clientKeys = append(clientKeys, priv.Public())
 	}
 
 	ln, err := net.Listen("tcp", ":0")
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer ln.Close()
 
 	var clientConns []net.Conn
-	for i := 0; i < numClients; i++ {
-		conn, err := net.Dial("tcp", ln.Addr().String())
-		if err != nil {
-			t.Fatal(err)
-		}
-		clientConns = append(clientConns, conn)
-	}
-	s := NewServer(serverPrivateKey, t.Logf)
-	defer s.Close()
-	for i := 0; i < numClients; i++ {
-		netConn, err := ln.Accept()
-		if err != nil {
-			t.Fatal(err)
-		}
-		conn := bufio.NewReadWriter(bufio.NewReader(netConn), bufio.NewWriter(netConn))
-		go s.Accept(netConn, conn)
-	}
-
 	var clients []*Client
 	var recvChs []chan []byte
 	errCh := make(chan error, 3)
+
 	for i := 0; i < numClients; i++ {
+		t.Logf("Connecting client %d ...", i)
+		cout, err := net.Dial("tcp", ln.Addr().String())
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer cout.Close()
+		clientConns = append(clientConns, cout)
+
+		cin, err := ln.Accept()
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer cin.Close()
+		go s.Accept(cin, bufio.NewReadWriter(bufio.NewReader(cin), bufio.NewWriter(cin)))
+
 		key := clientPrivateKeys[i]
-		netConn := clientConns[i]
-		conn := bufio.NewReadWriter(bufio.NewReader(netConn), bufio.NewWriter(netConn))
-		c, err := NewClient(key, netConn, conn, t.Logf)
+		brw := bufio.NewReadWriter(bufio.NewReader(cout), bufio.NewWriter(cout))
+		c, err := NewClient(key, cout, brw, t.Logf)
 		if err != nil {
 			t.Fatalf("client %d: %v", i, err)
 		}
 		clients = append(clients, c)
 		recvChs = append(recvChs, make(chan []byte))
+		t.Logf("Connected client %d.", i)
+	}
 
+	t.Logf("Starting read loops")
+	for i := 0; i < numClients; i++ {
 		go func(i int) {
 			for {
 				b := make([]byte, 1<<16)
-				n, err := c.Recv(b)
+				n, err := clients[i].Recv(b)
 				if err != nil {
 					errCh <- err
 					return
@@ -120,4 +124,7 @@ func TestSendRecv(t *testing.T) {
 	recv(2, string(msg2))
 	recvNothing(0)
 	recvNothing(1)
+
+	t.Logf("passed")
+	s.Close()
 }